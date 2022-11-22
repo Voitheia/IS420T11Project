204ecc6d9775d56db4aa3e1208c29ab45e34d708
zlivesa1 <116770799+zlivesa1@users.noreply.github.com>
2022-11-22 16:33:58 -0500

    Update group project.sql

--- group project.sql
+++ group project.sql
@@ -476,7 +476,7 @@ is
 begin
  for orders_rec in orders_cursor
  loop
- dbms_output.put_line('Waiter ID: ' || orders_rec.Waiter_ID || ', Accumulated Tips: ' || orders_rec.SumOfTips || chr(10));
+ dbms_output.put_line('Waiter ID: ' || orders_rec.Waiter_ID || ' | Accumulated Tips: ' || orders_rec.SumOfTips || chr(10));
  end loop;
  exception
      when no_data_found then
@@ -499,7 +499,7 @@ is
 begin
  for orders_rec in orders_cursor
  loop
- dbms_output.put_line('State: ' || orders_rec.RESTAURANT_STATE || ', Accumulated Tips: ' || orders_rec.SumOfTips || chr(10));
+ dbms_output.put_line('State: ' || orders_rec.RESTAURANT_STATE || ' | Accumulated Tips: ' || orders_rec.SumOfTips || chr(10));
  end loop;
  exception
      when no_data_found then
